6fb3ac404d505b9f1548902e4a14baea20145821
Rerric <79161388+Rerric@users.noreply.github.com>
2021-04-01 19:11:27 -0400

    Tweaked Spawn Manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject virusEnemyPrefab;
-    public GameObject pingEnemyPrefab;
-    public GameObject trojanEnemyPrefab;
-    public GameObject bossEnemyPrefab;
-
-
+    public List<GameObject> enemies;
+    private float wave;
+    private float spawnRate;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(virusEnemyPrefab, transform.position, transform.rotation);
-        Instantiate(pingEnemyPrefab, transform.position, transform.rotation);
-        Instantiate(trojanEnemyPrefab, transform.position, transform.rotation);
-        Instantiate(bossEnemyPrefab, transform.position, transform.rotation);
+        for (int i = 0; i < enemies.Count - 1; i++)
+        {
+            enemies[i].GetComponent<Enemy>().health += 100;
+        }
+
+        wave = 0;
+        spawnRate = 1.0f;
+
+        StartCoroutine(SpawnWave());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //make sure to update all present "Enemy" tagged objects' health and energy after every wave
+        
+    }
+
+    IEnumerator SpawnWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(enemies[1], transform.position, transform.rotation);
+        }
     }
 }
