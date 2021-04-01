2ebce2fbd9acfe761244d268155e0efaf1e94587
Rerric <79161388+Rerric@users.noreply.github.com>
2021-04-01 17:03:23 -0400

    Fiddled with Canvas and UI

---
+++ Assets/Scripts/EnergyCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyCounter : MonoBehaviour
+{
+    public int startingEnergy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
