44bd90acac50c93832ccd5ed3ee96bb62daf641c
Rerric <79161388+Rerric@users.noreply.github.com>
2021-04-05 18:13:42 -0400

    Quick Fixes

---
+++ Assets/Scripts/Motherboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Motherboard : MonoBehaviour
+{
+    public float startingHp;
+    public float hp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = startingHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
