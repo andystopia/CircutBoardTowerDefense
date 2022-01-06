1f69c0d3711c04b3e6352669e94880275fb2f21d
andystopia <amday25@colby.edu>
2022-01-06 18:18:19 -0500

    Added focus manager interfaces.

---
+++ Assets/Scripts/PrimitiveFocus/IExclusiveFocusInteractor.cs
@@ -0,0 +1,15 @@
+using ActiveOrInactiveStateManagement;
+
+namespace PrimitiveFocus
+{
+    public interface IExclusiveFocusInteractor : IFocusInteractor
+    {
+        void SetManager(ExclusiveSubsectionFocusManager manager);
+
+        /// <summary>
+        /// The more specific form if you have access to an instance of this class.
+        /// </summary>
+        /// <returns></returns>
+        ExclusiveSubsectionFocusManager GetFocusManager();
+    }
+}
