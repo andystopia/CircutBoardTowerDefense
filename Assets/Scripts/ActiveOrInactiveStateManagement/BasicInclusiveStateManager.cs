07a8fc991783d39c930a233b366e649b5b0c8636
andystopia <amday25@colby.edu>
2022-01-06 08:32:35 -0500

    Added a basic inclusive state manager.

---
+++ Assets/Scripts/ActiveOrInactiveStateManagement/BasicInclusiveStateManager.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace ActiveOrInactiveStateManagement
+{
+    public class BasicInclusiveStateManager<T> : MonoBehaviour,  IInclusiveStateManager<T> where T : class, IActiveOrInactiveState
+    {
+        protected ICollection<T> currentlyActive;
+        
+        public ICollection<T> GetActive()
+        {
+            return currentlyActive;
+        }
+
+        public void SetActive(T item)
+        {
+            if (item == null) return;
+            currentlyActive.Add(item);
+        }
+
+        public void InactivateAll()
+        {
+            currentlyActive = new Collection<T>();
+        }
+
+        public bool IsActive(T item)
+        {
+            return currentlyActive.Contains(item);
+        }
+    }
+}
